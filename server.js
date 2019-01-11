f0863ac36d1269534697aca869a7b9cc656f78f8
AlfredBryan <alfred.chimereze@gmail.com>
2019-01-11 10:57:51 +0100

    deploying

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const logger = require("morgan");
+const path = require("path");
 require("dotenv").config();
 
 const workersRoute = require("./routes/worker");
@@ -27,6 +28,16 @@ app.use(cors());
 
 app.use("/api", workersRoute);
 app.use("/api", userRoute);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server listening to port ${port}`);
